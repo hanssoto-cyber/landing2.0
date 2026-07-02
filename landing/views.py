b50e55a7ee8f9a0881e0ece9cd6434d4d0a2f4fb
hanssoto-cyber <hans.soto.g@gmail.com>
2026-07-02 13:28:23 -0400

    feat: contacto en home con envio de email via SMTP Gmail

--- landing/views.py
+++ landing/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from itertools import groupby
 from .models import Certificacion, Estadistica, Habilidad
-
+from proyectos.models import Proyecto
+from blog.models import Post
+from contacto.forms import MensajeForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 def home(request):
+    if request.method == 'POST':
+        form = MensajeForm(request.POST)
+        if form.is_valid():
+            mensaje = form.save()
+            send_mail(
+                subject=f'Nuevo mensaje de {mensaje.nombre}',
+                message=f'De: {mensaje.nombre} <{mensaje.email}>\n\n{mensaje.mensaje}',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[settings.EMAIL_HOST_USER],
+                fail_silently=False,
+            )
+            messages.success(request, '¡Mensaje enviado! Te responderé pronto.')
+            return redirect('/#contacto')
+    else:
+        form = MensajeForm()
+
     habilidades = Habilidad.objects.all()
-    # Agrupar por categoría (ya vienen ordenadas por categoria+orden desde el modelo)
     skills_por_categoria = []
     for categoria, items in groupby(habilidades, key=lambda h: h.get_categoria_display()):
         skills_por_categoria.append((categoria, list(items)))
@@ -14,5 +34,8 @@ def home(request):
         'certificaciones': Certificacion.objects.all(),
         'estadisticas': Estadistica.objects.all(),
         'skills_por_categoria': skills_por_categoria,
+        'proyectos': Proyecto.objects.all(),
+        'posts': Post.objects.filter(publicado=True)[:3],
+        'form': form,
     }
     return render(request, 'landing/home.html', context)
